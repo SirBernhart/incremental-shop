d687a31d330fab14cc57b8629283f2cc306baf20
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2026-07-20 08:20:44 -0300

    Create ClientQueueController to procedurally instantiate Clients

--- incremental shop/Assets/Scripts/GameSetup.cs
+++ incremental shop/Assets/Scripts/GameSetup.cs
@@ -1,12 +1,17 @@
+using System;
 using UnityEngine;
 using Utils;
 
 public class GameSetup : MonoBehaviour
 {
     [SerializeField] private PlayerInventory playerInventory;
+    [SerializeField] private ClientQueueController clientQueue;
     
     private void Awake()
     {
         ServicesLocator.Register<PlayerInventory>(playerInventory);
+
+        ServicesLocator.Register<ClientQueueController>(clientQueue);
+        clientQueue.Setup();
     }
 }
