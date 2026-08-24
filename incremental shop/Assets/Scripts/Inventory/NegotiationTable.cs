f027fa5f278b8246d5dc6b12297d95e1431df339
Bernardo <behornerl@gmail.com>
2026-08-24 18:25:16 -0300

    NegotiationTable.cs - Add method to clear all items

--- incremental shop/Assets/Scripts/Inventory/NegotiationTable.cs
+++ incremental shop/Assets/Scripts/Inventory/NegotiationTable.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using CoreGameplay.Items;
 using UnityEngine;
 
@@ -6,10 +7,21 @@ public class NegotiationTable : MonoBehaviour
     [SerializeField] protected Inventory playerInventory;
     [SerializeField] protected Transform itemHolder;
     [SerializeField] private ItemView itemViewPrefab;
+
+    private List<GameObject> items = new();
     
     public void AddItem(Item itemConfig)
     {
         ItemView itemView = Instantiate(itemViewPrefab, itemHolder);
         itemView.Setup(itemConfig, playerInventory);
+        items.Add(itemView.gameObject);
+    }
+
+    public void ClearItems()
+    {
+        foreach (GameObject item in items)
+        {
+            Destroy(item);
+        }
     }
 }
