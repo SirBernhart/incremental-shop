1c31be7a802701d0885cc63701b6b0aa256e9344
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2026-09-09 08:44:01 -0300

    DayPeriodController - add IsPeriodEnded interface

--- incremental shop/Assets/Scripts/Day/DayPeriodController.cs
+++ incremental shop/Assets/Scripts/Day/DayPeriodController.cs
@@ -16,6 +16,7 @@ public class DayPeriodController : MonoBehaviour
     public int GetTimerGoal => timerGoal;
 
     private int currentTime;
+    private int goalTime;
 
     private void Awake()
     {
@@ -38,10 +39,11 @@ public class DayPeriodController : MonoBehaviour
     {
         int tickMilliseconds = tickSeconds * 1000;
         int goalMilliseconds = goalSeconds * 1000;
+        goalTime = goalMilliseconds;
 
         currentTime = 0;
 
-        while(currentTime < goalMilliseconds)
+        while(IsPeriodEnded())
         {
             await UniTask.Delay(tickMilliseconds);
             currentTime += tickMilliseconds;
@@ -49,5 +51,10 @@ public class DayPeriodController : MonoBehaviour
         }
 
         OnPeriodEnd?.Invoke();
+    }
+
+    public bool IsPeriodEnded()
+    {
+        return currentTime < goalTime;
     }
 }
