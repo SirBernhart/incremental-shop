a5b8408806e6c2e96de76aa132d6be264fc498e0
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2026-09-21 07:38:41 -0300

    Client - add DraftItems to better fill buying and selling lists

--- incremental shop/Assets/Scripts/Clients/Client.cs
+++ incremental shop/Assets/Scripts/Clients/Client.cs
@@ -1,6 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using CoreGameplay.Items;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 /// <summary>
 /// Class that controls Clients and their respective requests
@@ -13,13 +15,58 @@ public class Client : MonoBehaviour
 
     public void Setup(NegotiationTable playerBuyingNegotiationTable, NegotiationTable playerSellingNegotiationTable)
     {
-        for (int i = 0; i < itemsToAddCount; i++)
+        (List<Item> buyingItems, List<Item> sellingItems) = DraftItems(itemsToAddCount, possibleItemsToAdd);
+
+        playerBuyingNegotiationTable.AddItemRange(buyingItems.ToArray());
+        playerSellingNegotiationTable.AddItemRange(sellingItems.ToArray());
+    }
+
+    private static (List<Item>, List<Item>) DraftItems(int totalItems, Item[] possibilityPool, bool guaranteeBothBuyAndSell = true)
+    {
+        var possibilityList = possibilityPool.ToList();
+
+        System.Random random = new System.Random(System.Guid.NewGuid().GetHashCode());
+
+        int separationValue = random.Next(0, Math.Min(totalItems, possibilityList.Count));
+        if(guaranteeBothBuyAndSell)
         {
-            Item draftedItemToSell = possibleItemsToAdd[Random.Range(0, possibleItemsToAdd.Length)];
-            playerBuyingNegotiationTable.AddItem(draftedItemToSell);
-            
-            Item draftedItemToBuy = possibleItemsToAdd[Random.Range(0, possibleItemsToAdd.Length)];
-            playerSellingNegotiationTable.AddItem(draftedItemToBuy);
+            if(separationValue == 0)
+            {
+                separationValue++;
+            }
+            if(separationValue != 1)
+            {
+                separationValue--;
+            }
+        }
+
+        int index = 0;
+
+        List<Item> buyingItems = new();
+        for(index = 0; index < separationValue; index++)
+        {
+            (Item draftedItemToSell, int chosenIndex) = draftItem(possibilityList, random);
+            buyingItems.Add(draftedItemToSell);
+
+            possibilityList.RemoveAt(chosenIndex);
+        }
+
+        List<Item> sellingItems = new();
+        for (; index < totalItems; index++)
+        {
+            (Item draftedItemToBuy, int chosenIndex) = draftItem(possibilityList, random);
+            sellingItems.Add(draftedItemToBuy);
+
+            possibilityList.RemoveAt(chosenIndex);
+        }
+
+        return (buyingItems, sellingItems);
+
+
+        static (Item, int) draftItem(List<Item> possibilityPool, System.Random random)
+        {
+            int index = random.Next(0, possibilityPool.Count);
+            return (possibilityPool[index], index);
         }
     }
 }
